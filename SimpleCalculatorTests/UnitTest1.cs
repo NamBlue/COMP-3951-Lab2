b8197f3a5807e353d04cb411713727ee452d657d
George Lee <namblue@me.com>
2017-02-11 13:57:41 -0800

    Fixed previous commit to add SimpleCalculatorTests.csproj

---
+++ SimpleCalculatorTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculator;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SimpleCalculator.SimpleCalculator calc = new SimpleCalculator.SimpleCalculator();
+            calc._operands.Add(3000.0);
+            calc._operands.Add(6000.0);
+            calc._operators.Add("+");
+            calc.calculate();
+            Assert.AreEqual("9000", calc._display.Text); 
+        }
+    }
+}
